3727cfcf4fccaf169bc25949bc833c35df50b8dd
Sam Hiatt <samhiatt@gmail.com>
2019-07-06 21:00:08 -0700

    Using zhash pool us-btg.2miners.com:4040

--- mine_zhash.sh
+++ mine_zhash.sh
@@ -4,7 +4,8 @@
 ADDRESS=GXRqRvbrc2ALe69gf88T5Y9ULTLBbYf3Ky
 
 USERNAME=$ADDRESS.w
-POOL=US.pool.gold:3050
+#POOL=US.pool.gold:3050
+POOL=us-btg.2miners.com:4040
 SCHEME=zhash
 
 ./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880
